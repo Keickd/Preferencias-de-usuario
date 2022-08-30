698c9aa3006026b7922be9a2e915b142dd495f31
Marcos Salas <keickd16@gmail.com>
2022-08-30 16:50:26 +0200

    basic side menu

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/widgets/side_menu.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -7,6 +8,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
+      drawer: SideMenu(),
       body: Center(
         child: Text('Hola settings'),
       ),
---
+++ lib/widgets/side_menu.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          _DrawerHeader(),
+          ListTile(
+            leading: Icon(Icons.pages_outlined),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, 'home');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.people_outline),
+            title: Text('People'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, 'people');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings_outlined),
+            title: Text('Settings'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, 'settings');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _DrawerHeader extends StatelessWidget {
+  const _DrawerHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      child: Container(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/menu-img.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
